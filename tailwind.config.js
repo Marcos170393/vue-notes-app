7e00ce09165b79da20300234dd78e54ea1d8b9b3
Dev_Marcos_1703 <mcorrea@ceibal.edu.uy>
2025-02-19 15:14:09 -0300

    deploy

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export const content = [];
-export const purge = ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
-
