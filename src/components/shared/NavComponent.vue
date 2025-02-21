5ba6e37e3a5efd315e0caf784c2a15ed84b73d59
Dev_Marcos_1703 <mcorrea@ceibal.edu.uy>
2025-02-21 16:53:35 -0300

    feat: logo

--- src/components/shared/NavComponent.vue
+++ src/components/shared/NavComponent.vue
@@ -2,6 +2,7 @@
     <div class="col-span-2 h-full w-full flex flex-col shadow-md shadow-slate-800 relative">
         <div class="flex flex-row mb-6 items-end border-b rounded-md border-slate-800 shadow-lg p-4 shadow-slate-600 mx-auto">
             <h5 class="font-sans text-xl antialiased font-semibold text-blue-gray-900">
+                <img src="../../resources/logo.png" class="size-7 inline-block " alt="">
                 MemoWave
             </h5>
         </div>
@@ -23,8 +24,6 @@
 <script setup>
     import NavButtonComponent from './NavButtonComponent.vue';
     import { links } from '../../config/nav-links';
-    import { BookOpenIcon } from '@heroicons/vue/16/solid';
     import { sendGitHubBrowser,sendLinkdeInBrowser } from '../../utils/open-link';
-    
 
 </script>
